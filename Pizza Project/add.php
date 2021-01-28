299d4f78e6d7f50ec2d8df5140d4cbf00a10e691
Arash-11 <arashnawy@gmail.com>
2021-01-27 20:43:29 -0400

    added form validation

---
+++ Pizza Project/add.php
@@ -0,0 +1,55 @@
+<?php
+
+  if (isset($_POST['submit'])) {
+
+    // check email
+    if (empty($_POST['email'])) {
+      echo 'An email is required <br />';
+    } else {
+      $email = $_POST['email'];
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "email must be a valid email address <br />";
+      }
+    }
+
+    // check title
+    if (empty($_POST['title'])) {
+      echo 'An title is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['title'])."<br />";
+    }
+
+    // check ingredients
+    if (empty($_POST['ingredients'])) {
+      echo 'At least one ingredient is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['ingredients'])."<br />";
+    }
+
+  } // end of POST check
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include('templates/header.php'); ?>
+
+  <section class="container grey-text">
+    <h4 class="center">Add a pizza</h4>
+    <form action="add.php" method="POST" class="white">
+      <label>Your Email:</label>
+      <input type="text" name="email">
+      <label>Pizza Title:</label>
+      <input type="text" name="title">
+      <label>Ingredients (comma separated):</label>
+      <input type="text" name="ingredients">
+      <div class="center">
+        <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+      </div>
+    </form>
+  </section>
+
+  <?php include('templates/footer.php'); ?>
+
+</html>
