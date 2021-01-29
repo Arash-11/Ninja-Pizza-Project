d8007871b9690f1e92b14e4721848ea51e48345d
Arash-11 <arashnawy@gmail.com>
2021-01-29 08:12:02 -0400

    connected php file to mysql database

--- Pizza Project/index.php
+++ Pizza Project/index.php
@@ -1,5 +1,12 @@
 <?php
 
+  // connect to database
+  $conn = mysqli_connect('localhost', 'arash', 'ninjapizza', 'ninja_pizza');
+
+  // check connection
+  if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+  }
 
 ?>
 
